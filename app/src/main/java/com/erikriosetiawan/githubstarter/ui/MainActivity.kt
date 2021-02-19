8363c24e0ec901c2727c41cb2fa77dea4004d534
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-19 11:24:07 +0700

    Hide support action bar

--- app/src/main/java/com/erikriosetiawan/githubstarter/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/githubstarter/ui/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
