75d4af0267918464a90263191396988e33b3ef63
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-19 10:37:44 +0700

    Integrated the view binding

--- app/src/main/java/com/erikriosetiawan/githubstarter/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/githubstarter/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.erikriosetiawan.githubstarter.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.erikriosetiawan.githubstarter.R
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.githubstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
