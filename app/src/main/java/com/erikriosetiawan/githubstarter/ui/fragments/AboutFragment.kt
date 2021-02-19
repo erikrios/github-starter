4dd27e26874700876d72b06decb4d8d38d38f2d4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-19 14:26:50 +0700

    Handle the view

--- app/src/main/java/com/erikriosetiawan/githubstarter/ui/fragments/AboutFragment.kt
+++ app/src/main/java/com/erikriosetiawan/githubstarter/ui/fragments/AboutFragment.kt
@@ -6,17 +6,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.erikriosetiawan.githubstarter.R
 import com.erikriosetiawan.githubstarter.databinding.FragmentAboutBinding
 import com.erikriosetiawan.githubstarter.models.User
+import com.erikriosetiawan.githubstarter.ui.viewmodels.MainViewModel
+import com.erikriosetiawan.githubstarter.ui.viewstate.UserViewState
 
 class AboutFragment : Fragment() {
 
     private var _binding: FragmentAboutBinding? = null
+    private val mainViewModel: MainViewModel by activityViewModels()
     private val binding get() = _binding
 
     override fun onCreateView(
@@ -29,25 +34,38 @@ class AboutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val user = User(
-            id = 42199285,
-            name = "Erik Rio Setiawan",
-            username = "erikrios",
-            avatarUrl = "https://avatars.githubusercontent.com/u/42199285?v=4",
-            htmlUrl = "https://github.com/erikrios",
-            company = "Student at Muhammadiyah University of Ponorogo",
-            location = "Ponorogo, East Java, Indonesia",
-            followers = 13,
-            following = 49,
-            publicRepositories = 405
-        )
-        handleView(user)
+        mainViewModel.apply {
+            val username = getString(R.string.my_github_username)
+            getUserDetails(username)
+            userViewState.observe(viewLifecycleOwner, this@AboutFragment::handleState)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun handleState(viewState: UserViewState) {
+        handleLoading(viewState.loading)
+        viewState.exception?.let { handleError(it) }
+        viewState.user?.let { handleSuccess(it) }
+    }
+
+    private fun handleLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding?.progressBar?.visibility = View.VISIBLE
+            binding?.appBarLayout?.visibility = View.INVISIBLE
+        } else {
+            binding?.progressBar?.visibility = View.GONE
+            binding?.appBarLayout?.visibility = View.VISIBLE
+        }
+    }
+
+    private fun handleError(exception: Exception) =
+        Toast.makeText(context, exception.message, Toast.LENGTH_SHORT).show()
+
+    private fun handleSuccess(user: User) = handleView(user)
 
     private fun openInBrowser(user: User) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(user.htmlUrl))
