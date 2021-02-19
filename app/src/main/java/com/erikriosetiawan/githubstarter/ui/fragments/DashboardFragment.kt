85b58d58c0fda67b06407b9a4a968f5a3be982d0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-19 11:49:55 +0700

    Add a method to set recycler view

--- app/src/main/java/com/erikriosetiawan/githubstarter/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/erikriosetiawan/githubstarter/ui/fragments/DashboardFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.erikriosetiawan.githubstarter.adapters.UserAdapter
 import com.erikriosetiawan.githubstarter.databinding.FragmentDashboardBinding
+import com.erikriosetiawan.githubstarter.models.User
 
 class DashboardFragment : Fragment() {
 
@@ -20,8 +23,38 @@ class DashboardFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView(generateUsers())
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setRecyclerView(users: List<User>) {
+        val adapter = UserAdapter(users)
+        adapter.setOnItemClickListener { user ->
+            val action = DashboardFragmentDirections.actionDashboardFragmentToDetailsFragment(user)
+            findNavController().navigate(action)
+        }
+        binding?.rvUsers?.adapter = adapter
+    }
+
+    private fun generateUsers(): List<User> {
+        val users = mutableListOf<User>()
+
+        for (i in 0..50) {
+            val user = User(
+                username = "username $i",
+                htmlUrl = "https://github.com/erikrios",
+                id = i.toLong(),
+                avatarUrl = "https://avatars.githubusercontent.com/u/42199285?v=4"
+            )
+            users.add(user)
+        }
+
+        return users
     }
 }
